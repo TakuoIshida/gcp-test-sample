74cff3f81d9c9506d1231667649e7539992a4f9d
Tishida <archersparadox.100@gmail.com>
2023-11-13 22:36:46 +0900

    環境変数をセット

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/tasks/create')
+  createTask(): string {
+    return this.appService.createTasks();
+  }
+
+  @Post('/tasks/handle')
+  handleTasks(): string {
+    return 'tasks completed';
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    return `Hello World!${process.env.PROJECT_ID}`;
+  }
+
+  createTasks(): string {
     return 'Hello World!';
   }
 }
