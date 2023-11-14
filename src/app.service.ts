0fc6b604261d943131d3d2978d9c76c055f2854a
Tishida <archersparadox.100@gmail.com>
2023-11-14 10:14:23 +0900

    yamlファイルに環境変数をセット

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World!${process.env.PROJECT_ID}`;
+    return `Hello World!${process.env.CLOUD_TASKS_QUEUE_NAME}`;
   }
 
   createTasks(): string {
