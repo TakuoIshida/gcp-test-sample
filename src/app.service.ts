4d6f72d6b7841a0ea85b19d3b181fad2a12bfc3c
Tishida <archersparadox.100@gmail.com>
2023-11-14 17:20:24 +0900

    環境変数のテスト

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World!${process.env.CLOUD_TASKS_QUEUE_NAME}`;
+    return `Hello World!${process.env.CLOUD_TASKS_QUEUE_NAME},${process.env.HOGE}`;
   }
 
   createTasks(): string {
